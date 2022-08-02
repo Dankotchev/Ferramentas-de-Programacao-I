68555fea5592664156c141e6792c2c075b06b14f
Dankotchev <dankotchev.tech@outlook.com>
2022-08-02 13:09:37 -0300

    Continuação da Aula Inaugural

--- FP1C4 - 00 Aulas Iniciais/Aula Inagural/src/br/edu/ifsp/pep/aulainalgural/Principal.java
+++ FP1C4 - 00 Aulas Iniciais/Aula Inagural/src/br/edu/ifsp/pep/aulainalgural/Principal.java
@@ -1,9 +1,31 @@
 package br.edu.ifsp.pep.aulainalgural;
 
+import br.edu.ifsp.pep.aulainalgural.modelo.Pessoa;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Principal {
+
     public static void main(String[] args) {
-        Persistence.createEntityManagerFactory("FP1C4Aula01PU");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FP1C4Aula01PU");
+
+//        Pessoa pessoa = new Pessoa();
+//        pessoa.setDataNascimento(new Date());
+//        pessoa.setNome("Danilo");
+//        pessoa.setSalario(3000.656);                                            // 0000.d pois o valor é um Double
+
+        Pessoa pessoa = new Pessoa("Danilo", new BigDecimal(3520.6589), new Date());
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();                                            // Iniciar uma transação
+
+//        em.persist(pessoa);     // Insert (lá no Banco de Dados)
+        em.merge(pessoa);       // Update se objeto existir, senão Insert
+
+        em.getTransaction().commit();                                           // Finaliza uma transação
     }
 }
--- FP1C4 - 00 Aulas Iniciais/Aula Inagural/src/br/edu/ifsp/pep/aulainalgural/modelo/Pessoa.java
+++ FP1C4 - 00 Aulas Iniciais/Aula Inagural/src/br/edu/ifsp/pep/aulainalgural/modelo/Pessoa.java
@@ -1,20 +1,35 @@
 package br.edu.ifsp.pep.aulainalgural.modelo;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
 
+
+// Toda entidade tem que ter o construtor vazio
 @Entity
 @Table(name = "pessoa")     // Define o nome da tabela associada a essa classe
 public class Pessoa {
-    @Id     // Define a linha abaixo como chave primária para o Banco de Dados
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // DEfinir como será gerado os códigos (auto incremento)
+    @Id                                                         // Define a linha abaixo como chave primária para o Banco de Dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY)         // Definir como será gerado os códigos (auto incremento)
+    @Column(name = "codigo", nullable = false)                  // Definindo o nome do atributo e caracteristicas no Banco de Dados
     private Long codigo;
+    
+    @Column(name = "nome", length = 40, nullable = false)       // Define parametros do atributos: length = tamanho do varchar
     private String nome;
-    private Double salario;
+    
+                                                                // Precision (tamanho total de digitos) Scale (tamanho de casas decimais)
+                                                                // BigDecimal é o necessário para ter essa precisão no Banco de Dados
+    @Column(name = "salario", precision = 8, scale = 2)        
+    private BigDecimal salario;
+    
+    @Column(name = "data_nascimento", nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataNascimento;
 
     public Long getCodigo() {
@@ -33,11 +48,11 @@ public class Pessoa {
         this.nome = nome;
     }
 
-    public Double getSalario() {
+    public BigDecimal getSalario() {
         return salario;
     }
 
-    public void setSalario(Double salario) {
+    public void setSalario(BigDecimal salario) {
         this.salario = salario;
     }
 
@@ -48,4 +63,16 @@ public class Pessoa {
     public void setDataNascimento(Date dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+
+    // Esse construtor pode ser usado no código sem problemas
+    public Pessoa(String nome, BigDecimal salario, Date dataNascimento) {
+        this.nome = nome;
+        this.salario = salario;
+        this.dataNascimento = dataNascimento;
+    }
+
+    // Mas o construtor vazio é necessário para o JPA
+    public Pessoa() {
+    }
+    
 }
