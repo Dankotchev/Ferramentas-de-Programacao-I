4e62e38d9cd22d276911fd358a4d7bba1f4db56a
Dankotchev <dankotchev.tech@outlook.com>
2022-08-26 00:00:29 -0300

    Sala de Aula: 2022-08-25 -- Abstraindo classes DAO usando uma Abstract

--- FP1C4 - 00 Aulas Iniciais/Tabelas com Relacionamentos/src/br/edu/ifsp/pep/relacionamento/dao/CategoriaDAO.java
+++ FP1C4 - 00 Aulas Iniciais/Tabelas com Relacionamentos/src/br/edu/ifsp/pep/relacionamento/dao/CategoriaDAO.java
@@ -2,43 +2,15 @@ package br.edu.ifsp.pep.relacionamento.dao;
 
 import br.edu.ifsp.pep.relacionamento.modelo.Categoria;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
-public class CategoriaDAO {
-
-    private EntityManagerFactory emf;
-
-    // Cria a "fabríca" ao iniciar a classe
-    public CategoriaDAO() {
-        emf = Persistence.createEntityManagerFactory("TR_PU");
-    }
-
-    // Cria o "gerente" sempre que for necessário
-    private EntityManager getEntityManager() {
-        return this.emf.createEntityManager();
-    }
-
-    public void inserir(Categoria categoria) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.persist(categoria);
-        em.getTransaction().commit();
-    }
-
-    public void alterar(Categoria categoria) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.merge(categoria);
-        em.getTransaction().commit();
-    }
+public class CategoriaDAO extends AbstractDAO<Categoria> {
 
     // Retornando o resultado da consulta em uma lista de Produtos
     public List<Categoria> buscar() {
         // Retornando a consulta: Pensar em classes
-        TypedQuery<Categoria> query = getEntityManager().createQuery("SELECT c FROM Categoria c", Categoria.class);
+        TypedQuery<Categoria> query = super.getEntityManager().
+                createQuery("SELECT c FROM Categoria c", Categoria.class);
         return query.getResultList();
     }
 }
--- FP1C4 - 00 Aulas Iniciais/Tabelas com Relacionamentos/src/br/edu/ifsp/pep/relacionamento/dao/ProdutoDAO.java
+++ FP1C4 - 00 Aulas Iniciais/Tabelas com Relacionamentos/src/br/edu/ifsp/pep/relacionamento/dao/ProdutoDAO.java
@@ -2,43 +2,15 @@ package br.edu.ifsp.pep.relacionamento.dao;
 
 import br.edu.ifsp.pep.relacionamento.modelo.Produto;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
-public class ProdutoDAO {
-
-    private EntityManagerFactory emf;
-
-    // Cria a "fabríca" ao iniciar a classe
-    public ProdutoDAO() {
-        emf = Persistence.createEntityManagerFactory("TR_PU");
-    }
-
-    // Cria o "gerente" sempre que for necessário
-    private EntityManager getEntityManager() {
-        return this.emf.createEntityManager();
-    }
-
-    public void inserir(Produto produto) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.persist(produto);
-        em.getTransaction().commit();
-    }
-
-    public void alterar(Produto produto) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.merge(produto);
-        em.getTransaction().commit();
-    }
+public class ProdutoDAO extends AbstractDAO<Produto> {
 
     // Retornando o resultado da consulta em uma lista de Produtos
     public List<Produto> buscar() {
         // Retornando a consulta: Pensar em classes
-        TypedQuery<Produto> query = getEntityManager().createQuery("SELECT p FROM Produto p", Produto.class);
+        TypedQuery<Produto> query = super.getEntityManager().
+                createQuery("SELECT p FROM Produto p", Produto.class);
         return query.getResultList();
     }
 }
