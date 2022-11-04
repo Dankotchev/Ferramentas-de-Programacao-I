375bdaec413e1d391974daf628bc0c6c27cf6540
Dankotchev <dankotchev.tech@outlook.com>
2022-11-03 21:01:07 -0300

    Atualizando Mensagem e Inclussão da Classe de Relátorios

--- FP1C4 - 03 Interfaces Gráficas/01 Locadora de Veiculos/src/br/edu/ifsp/pep/utilitarios/Mensagem.java
+++ FP1C4 - 03 Interfaces Gráficas/01 Locadora de Veiculos/src/br/edu/ifsp/pep/utilitarios/Mensagem.java
@@ -1,30 +1,30 @@
-package br.edu.ifsp.pep.locadoraveiculo.utilitarios;
+package br.edu.ifsp.pep.utilitarios;
 
 import javax.swing.JOptionPane;
 
 public class Mensagem {
 
-    public void mAviso(String mensagem) {
+    public static void mAviso(String mensagem) {
         JOptionPane.showMessageDialog(null, mensagem, "Aviso", JOptionPane.WARNING_MESSAGE);
     }
 
-    public void mAtencao(String mensagem) {
+    public static void mAtencao(String mensagem) {
         JOptionPane.showMessageDialog(null, mensagem, "Atenção", JOptionPane.WARNING_MESSAGE);
     }
 
-    public void mErro(String mensagem) {
+    public static void mErro(String mensagem) {
         JOptionPane.showMessageDialog(null, mensagem, "Erro", JOptionPane.ERROR_MESSAGE);
     }
 
-    public void mCorreto(String mensagem) {
+    public static void mCorreto(String mensagem) {
         JOptionPane.showMessageDialog(null, mensagem);
     }
-    
-    public void mInformacao(String mensagem){
+
+    public static void mInformacao(String mensagem) {
         JOptionPane.showMessageDialog(null, mensagem, "Informação", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    public boolean mSimNao(String mensagem) {
+    public static boolean mSimNao(String mensagem) {
         boolean retorno = true;
         int escolha = JOptionPane.showConfirmDialog(null, mensagem,
                 "Atenção", JOptionPane.YES_NO_OPTION);
@@ -34,14 +34,14 @@ public class Mensagem {
         return retorno;
     }
 
-    public boolean mConfimarCancelar(String mensagem) {
+    public static boolean mConfimarCancelar(String mensagem) {
         boolean retorno = true;
-        int escolha = JOptionPane.showConfirmDialog(null, mensagem, 
+        int escolha = JOptionPane.showConfirmDialog(null, mensagem,
                 "Atenção", JOptionPane.OK_CANCEL_OPTION);
         if (escolha == JOptionPane.CANCEL_OPTION) {
             retorno = false;
         }
         return retorno;
     }
-    
+
 }
---
+++ FP1C4 - 03 Interfaces Gráficas/01 Locadora de Veiculos/src/br/edu/ifsp/pep/utilitarios/Relatorio.java
@@ -0,0 +1,59 @@
+package br.edu.ifsp.pep.utilitarios;
+
+import java.io.InputStream;
+import java.util.List;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class Relatorio {
+
+    public static void gerarFromXML(String fileXML, List lista) {
+        try {
+            // Gera um data source a partir da lista
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(lista);
+
+            // Carrega o relatório
+            InputStream relatorioCompilado = Relatorio.class
+                    .getResourceAsStream("/br/edu/ifsp/pep/relatorio/" + fileXML);
+
+            // Complilando o relatório
+            JasperReport jasperReport = JasperCompileManager
+                    .compileReport(JRXmlLoader.load(relatorioCompilado));
+
+            // Preenche o relátorio com os dados
+            JasperPrint jp = JasperFillManager.fillReport(jasperReport, null, dataSource);
+
+            // Exibe o relatório
+            JasperViewer visualizador = new JasperViewer(jp, true);
+            visualizador.setVisible(true);
+        } catch (JRException ex) {
+            Mensagem.mErro(ex.getMessage());
+        }
+    }
+    
+    public static void gerarFromJasper(String fileJasper, List lista) {
+        try {
+            // Gera um data source a partir da lista
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(lista);
+
+            // Carrega o relatório
+            InputStream relatorioCompilado = Relatorio.class
+                    .getResourceAsStream("/br/edu/ifsp/pep/relatorio/" + fileJasper);
+
+            // Preenche o relátorio com os dados
+            JasperPrint jp = JasperFillManager.fillReport(relatorioCompilado, null, dataSource);
+
+            // Exibe o relatório
+            JasperViewer visualizador = new JasperViewer(jp, true);
+            visualizador.setVisible(true);
+        } catch (JRException ex) {
+            Mensagem.mErro(ex.getMessage());
+        }
+    }
+}
